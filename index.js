6e4cc54245ea26a2f8adc4b724c65d574a172eb9
Shinono <85401098+K93MSY@users.noreply.github.com>
2022-08-21 19:34:35 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+var app = require('express')();
+const express = require("express");
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 80;
+
+let message = [];
+
+app.use(express.static(__dirname + "/public"), (_, res, next) => {
+    res.status(404)
+    res.sendFile(__dirname + "/public/index.html")
+  });
+
+
+io.on('connection', function(socket){
+    let datalong = Object.keys(message).length;
+    io.emit('reset chat', 'message updated');
+    for(let i=0;i<datalong;i++){
+        console.log(message[i].message);
+        io.emit('chat message', message[i]);
+    }
+  socket.on('chat message', function(msg){
+    message.push({message:msg});
+    let datalong = Object.keys(message).length;
+    io.emit('reset chat', 'message updated');
+    for(let i=0;i<datalong;i++){
+        console.log(message[i].message);
+        io.emit('chat message', message[i]);
+    }
+  });
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
