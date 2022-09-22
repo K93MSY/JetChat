c7ad67deff442d28e6e84423bed5ec6bb7621c49
K93MSY <masaya93@ogaki-tv.ne.jp>
2022-09-22 22:18:04 +0900

    uuid追加(リログしたら変わる)

--- index.js
+++ index.js
@@ -2,13 +2,34 @@ var app = require('express')();
 const express = require("express");
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+const fs = require("fs");
+const { v4: uuidv4 } = require('uuid');
 var port = process.env.PORT || 80;
 
 let message = [];
+let uuid_list = [];
+
+function writeFile(path, data) {
+  const jsonStr = JSON.stringify(data);
+  fs.writeFile(path, jsonStr, (err) => {
+    if (err) rej(err);
+    if (!err) {
+      //console.log('JsonUpdated');
+      //console.log(data);
+    }
+  });
+}
 
 app.use(express.static(__dirname + "/public"), (_, res, next) => {
-    res.status(200)
-    res.sendFile(__dirname + "/public/index.html")
+  let uid = uuidv4();
+  console.log(uid);
+  uuid_list.push(uid);
+  writeFile('./log/uuid.json',uuid_list)
+    res.cookie('uuid',uid, {
+      maxAge: 1000*60*60*24,
+    })
+  res.status(200)
+  res.sendFile(__dirname + "/public/index.html")
   });
 
 
@@ -29,6 +50,7 @@ io.on('connection', function(socket){
     console.log(msg)
 
     message.push(msg);
+    writeFile('./log/log.json',message)
 
     let datalong = Object.keys(message).length;
 
--- public/script.js
+++ public/script.js
@@ -1,9 +1,23 @@
 $(function () {
-    var socket = io();
+    var uuid;
+    var cookies = document.cookie; //全てのcookieを取り出して
+    var cookiesArray = cookies.split(';'); // ;で分割し配列に
+    
+    for(var c of cookiesArray){ //一つ一つ取り出して
+        var cArray = c.split('='); //さらに=で分割して配列に
+        if( cArray[0] == 'uuid'){ // 取り出したいkeyと合致したら
+            console.log(cArray);  // [key,value] 
+            $(".urid").html(cArray[1]);
+            uuid = cArray[1];
+        }
+    }
+
+    var socket = io("ws://localhost:80");
     $('form').submit(function(){
       let send_data = {
         name:$('#name').val(),
         main:$('#textarea').val(),
+        uuid:uuid,
       }
       socket.emit('chat message', send_data);
       $('#textarea').val('');
@@ -16,7 +30,7 @@ $(function () {
 
     socket.on('chat message', function(msg){
         console.log(msg.main);
-      $('#messages').append($('<div class="sol"><span class="u_name">'+msg.name+"</span><br><a class='u_msg'>"+msg.main+"</a>"));
+      $('#messages').append($('<div class="sol"><span class="u_name">'+msg.name+"</span><span class='u_uuid'>id:"+msg.uuid+"</span><br><a class='u_msg'>"+msg.main+"</a>"));
       window.scrollTo(0, document.body.scrollHeight);
     });
   });
